0d467c8a7ad40fc402230c26668504f026f16c11
Joshy Joseph <jojoseph@microsoft.com>
2019-07-24 09:55:01 -0700

    Caching Strategy Improvement to avoid Restart

--- ApplicationCore/Interfaces/IUserProfileRepository.cs
+++ ApplicationCore/Interfaces/IUserProfileRepository.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright(c) Microsoft Corporation. 
 // All rights reserved.
-//
+// 
 // Licensed under the MIT license. See LICENSE file in the solution root folder for full license information
 
 using System.Collections.Generic;
